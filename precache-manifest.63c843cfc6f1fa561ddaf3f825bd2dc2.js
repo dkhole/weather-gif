67f2030b75e2443332ef27f8460b00ca55478eaa
dkhole <dkhlee419@gmail.com>
2020-09-20 14:30:19 +1000

    Updates

---
+++ precache-manifest.63c843cfc6f1fa561ddaf3f825bd2dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10afa24c3390696061745c1d15601e1",
+    "url": "/weather-gif/index.html"
+  },
+  {
+    "revision": "40df76861d88b6a74018",
+    "url": "/weather-gif/static/css/main.64dbe277.chunk.css"
+  },
+  {
+    "revision": "8b852b32c2d8fcd40161",
+    "url": "/weather-gif/static/js/2.6ce1f45a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-gif/static/js/2.6ce1f45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40df76861d88b6a74018",
+    "url": "/weather-gif/static/js/main.04c85790.chunk.js"
+  },
+  {
+    "revision": "c0cd33bce6a88f431241",
+    "url": "/weather-gif/static/js/runtime-main.78bd4db0.js"
+  }
+]);
